2ecf684862e1be5da94b0eab06b8c7a3e2e5c264
Martijn Olthuis <martijn.olthuis2@gmail.com>
2025-10-21 14:19:14 +0200

    test: add FormInput to ContactView

--- frontend/src/views/ContactView.vue
+++ frontend/src/views/ContactView.vue
@@ -1,19 +1,36 @@
 <template>
   <div class="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
-    <h1 class="text-3xl font-bold mb-6">Test 1: Adding AppHeader</h1>
+    <h1 class="text-3xl font-bold mb-6">Test 2: Adding FormInput</h1>
 
     <AppHeader
       :title="$t('contact.title')"
       page-context="contact"
     />
 
-    <p class="text-lg mb-4 mt-6">If you see the header above, AppHeader works!</p>
+    <div class="mt-6 bg-white p-6 rounded-lg shadow">
+      <FormInput
+        id="name"
+        v-model="form.name"
+        type="text"
+        :label="$t('contact.form.nameLabel')"
+        :placeholder="$t('contact.form.namePlaceholder')"
+        :required="true"
+      />
+    </div>
+
+    <p class="text-lg mb-4 mt-6">If you see the form input above, FormInput works!</p>
   </div>
 </template>
 
 <script setup>
+import { ref, reactive } from 'vue'
 import { useI18n } from 'vue-i18n'
 import AppHeader from '../components/shared/AppHeader.vue'
+import FormInput from '../components/shared/FormInput.vue'
 
 const { t } = useI18n()
+
+const form = reactive({
+  name: ''
+})
 </script>
